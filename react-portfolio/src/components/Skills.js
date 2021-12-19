781f07403c26ab597fe630891d6dc333730b54d7
Jay Leach <leachcoding@gmail.com>
2021-12-18 18:14:50 -0600

    Skills technology edits

--- react-portfolio/src/components/Skills.js
+++ react-portfolio/src/components/Skills.js
@@ -2,6 +2,59 @@ import React from 'react';
 import './Skills.css';
 
 const Skills = () => {
+  const languages = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'Python',
+    'SQL'
+  ];
+  const librariesFrameworksDatabases = [
+    'Angular.js',
+    'Bootstrap',
+    'Vue.js',
+    'Vuetify',
+    'Flask',
+    'Jinja',
+    'Postgresql',
+    'MySQL',
+    'SQLAlchemy',
+    'React.js',
+    'Redux',
+    'LESS',
+    'SASS',
+    'Styled-Components',
+    'Express.js',
+    'Node.js',
+    'Knex.js',
+    'bcrypt.js',
+    'Jest',
+    'Cypress'
+  ];
+  const tools = [
+    'AWS',
+    'Elastic Beanstalk',
+    'S3',
+    'EC2',
+    'AWS RDS',
+    'Git',
+    'Github',
+    'Netlify',
+    'Heroku',
+    'Atom',
+    'Gitbash',
+    'Postman',
+    'SQLiteStudio',
+  ];
+  const relatedSkills = [
+    'Object Oriented Programming',
+    'Test Driven Development',
+    'Pair Programming',
+    'AGILE Development',
+    'RESTful APIS',
+    'Project Management'
+  ];
+
   return (
     <>
       <div className='skills'>
@@ -15,28 +68,18 @@ const Skills = () => {
           <div className='languages'>
             <h4>Languages</h4>
             <ul>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>Python</li>
-              <li>SQL</li>
+              {languages.map((language) => {
+                return <li>{language}</li>
+              })}
             </ul>
           </div>
 
           <div className='libraries'>
             <h4>Libraries/Frameworks</h4>
             <ul>
-              <li>React.js</li>
-              <li>Redux</li>
-              <li>LESS</li>
-              <li>SASS</li>
-              <li>Styled-Components</li>
-              <li>Express.js</li>
-              <li>Node.js</li>
-              <li>Knex.js</li>
-              <li>bcrypt.js</li>
-              <li>Jest</li>
-              <li>Cypress</li>
+              {librariesFrameworksDatabases.map((listItem) => {
+                return <li>{listItem}</li>
+              })}
             </ul>
           </div>
 
